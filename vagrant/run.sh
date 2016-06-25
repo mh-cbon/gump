a5a802781eb16f8d151105c7190b5dd27e4a5879
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-25 16:19:38 +0200

    update release scripts

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -2,3 +2,4 @@
 go build -o build/gump gump.go
 
 vagrant ssh -c "sh /vagrant/vagrant/git.sh && cd ~/gow/src/github.com/mh-cbon/gump && GO15VENDOREXPERIMENT=1 GOPATH=/home/vagrant/gow GOROOT=/home/vagrant/go ~/go/bin/go test"
+vagrant ssh -c "cd ~/gow/src/github.com/mh-cbon/gump/config && GO15VENDOREXPERIMENT=1 GOPATH=/home/vagrant/gow GOROOT=/home/vagrant/go ~/go/bin/go test"
