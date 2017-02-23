84566682c8b88e6c6b26a91c9ce46d1bc1357174
mh-cbon <mh-cbon@users.noreply.github.com>
2017-02-23 16:44:50 +0100

    update to go1.8

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,9 +1,9 @@
 sudo apt-get install -y curl make binutils bison gcc build-essential
 sudo apt-get install -y git
 
-if [ ! -f go1.6.2.linux-amd64.tar.gz ]; then
-  curl -s -S -L https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz -o go1.6.2.linux-amd64.tar.gz
-  tar -xf go1.6.2.linux-amd64.tar.gz
+if [ ! -f go1.8.linux-amd64.tar.gz ]; then
+  curl -s -S -L https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz -o go1.8.linux-amd64.tar.gz
+  tar -xf go1.8.linux-amd64.tar.gz
 fi
 
 mkdir -p ~/gow/src/github.com/mh-cbon
