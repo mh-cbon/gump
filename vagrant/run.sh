c4ada5d6d5fb8b0c30d0e2f0d4d7c6706d95b3cc
mh-cbon <mh-cbon@users.noreply.github.com>
2017-02-23 18:37:57 +0100

    windows: fix tests for windows

--- vagrant/run.sh
+++
@@ -1,7 +0,0 @@
-
-go build -o build/gump gump.go
-
-GUMPDIR="~/gow/src/github.com/mh-cbon/gump"
-GOBIN="GOPATH=/home/vagrant/gow GOROOT=/home/vagrant/go ~/go/bin/go"
-
-vagrant ssh -c "sh /vagrant/vagrant/git.sh && cd $GUMPDIR && $GOBIN test && cd $GUMPDIR/config && $GOBIN test"
--- vagrant/test.sh
+++
@@ -1,6 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
-vagrant init ubuntu/trusty64 || echo "has already init"
-vagrant up
-vagrant ssh -c "sh /vagrant/vagrant/setup.sh"
-sh $DIR/run.sh
